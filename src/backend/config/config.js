a1a3e51c30233af82791ba85a4783f118cd5e7eb
devamontariotech <devamdharmendrabhai.shah@ontariotechu.net>
2025-03-01 03:36:55 +0530

    Changed config.js file.

--- src/backend/config/config.js
+++ src/backend/config/config.js
@@ -1,3 +1,3 @@
 require("dotenv").config();
-const appPath = process.env.APP_PATH || "./src";
+const appPath = process.env.APP_PATH ? path.normalize(process.env.APP_PATH) : "./src";
 console.log("App Path : ",appPath);
