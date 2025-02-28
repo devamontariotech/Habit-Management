1407a7c81cdaa757585f0dffd61ef9d680b3cec8
devamontariotech <devamdharmendrabhai.shah@ontariotechu.net>
2025-03-01 03:47:40 +0530

    Configured the app.js

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const fs = require('fs').promises;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Habit Tracking App');
-});
+// app.get('/', (req, res) => {
+//   res.send('Habit Tracking App');
+// });
+
+app.get('/src/frontend/pages/homepage',(req,res) => {
+  res.send("Habit tracking app")
+})
 
 app.listen(3000, () => console.log('Running on http://localhost:3000'));
