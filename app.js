78fe12b9ce4363dde8d6a254f52de7efdd202611
devamontariotech <devamdharmendrabhai.shah@ontariotechu.net>
2025-03-01 22:39:29 +0530

    Today's commit!

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
+require("dotenv").config();
 const express = require('express');
 const router = express.Router();
 const path = require('path');
 const fs = require('fs').promises;
 
 const app = express();
+const appPath = process.env.APP_PATH ? path.resolve(process.env.APP_PATH) : path.join(__dirname,"src");
 
-// app.get('/', (req, res) => {
-//   res.send('Habit Tracking App');
-// });
-
-app.get('/src/frontend/pages/homepage',(req,res) => {
-  res.send("Habit tracking app")
+app.use(express.static(appPath));
+app.get("",(req,res) => {
+  res.sendFile(path.join(appPath,"/frontend/pages/homepage.html"));
 })
 
 app.listen(3000, () => console.log('Running on http://localhost:3000'));
